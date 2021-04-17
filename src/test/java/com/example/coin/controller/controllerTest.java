4dea67439804b71718fab068bb94c7258e4d24f3
Kyabia1225 <kyabia@foxmail.com>
2021-04-18 01:40:41 +0800

    增加controller 的test

--- src/test/java/com/example/coin/controller/controllerTest.java
+++ src/test/java/com/example/coin/controller/controllerTest.java
@@ -4,6 +4,8 @@ import com.example.coin.DAO.EntityRepository;
 import com.example.coin.DAO.RelationRepository;
 import com.example.coin.po.Entity;
 import com.example.coin.util.RedisUtil;
+import com.example.coin.util.ResponseVO;
+import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +13,8 @@ import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -29,7 +33,12 @@ public class controllerTest {
 
     @Test
     public void Test01(){
-
+        ResponseVO response1 = entityController.addEntity(new Entity("yuzijiang", "person"));
+        String id = ((Entity) response1.getContent()).getId();
+        Assert.assertTrue(entityController.getEntityById(id).getSuccess());
+        entityController.deleteEntityById(id);
+        Assert.assertFalse(entityController.getEntityById(id).getSuccess());
+        Assert.assertEquals(0, ((List<Entity>)entityController.getEntityList().getContent()).size());
     }
 
     @Test
