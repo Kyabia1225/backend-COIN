d5a1acf30e75c99d317cc3e0a039164448c5fb19
WangShi <181250141@smail.nju.edu.cn>
2021-06-18 20:05:36 +0800

    question controller

---
+++ src/main/java/com/example/coin/controller/QuestionController.java
@@ -0,0 +1,22 @@
+package com.example.coin.controller;
+
+import com.example.coin.po.Entity;
+import com.example.coin.service.QuestionService;
+import com.example.coin.util.ResponseVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/coin")
+public class QuestionController {
+    @Autowired
+    private QuestionService questionService;
+
+    @RequestMapping(path = "/question",method = RequestMethod.GET)
+    public ResponseVO addEntity(@RequestParam String question)  throws Exception{
+        String answer = questionService.answer(question);
+        if(answer.equals(""))
+            return ResponseVO.buildFailure("无法理解你的问题");
+        return ResponseVO.bulidSuccess(answer);
+    }
+}
